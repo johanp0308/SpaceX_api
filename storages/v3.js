2a65b549435d80a628c6b27e8913dea19c54ffd1
johanp0308 <111256873+johanp0308@users.noreply.github.com>
2023-09-26 22:42:55 -0500

    feat: :beers: Consultation of images of little boats and their names
    Consultation of images of little boats and their names
    
    @johanp0308

--- storages/v3.js
+++ storages/v3.js
@@ -1,9 +1,32 @@
-export const getAllImgLaunches = async () =>{
+const getAllImgLaunches = async () =>{
     let res = await (await fetch("https://api.spacexdata.com/v3/launches")).json();
     let img = res.map(ele => {
         let {links: lk, links: {flickr_images: r}} = ele;
         return r;
     });
     return img;
+}
+
+const getAllImgRockets = async () =>{
+    let res = await (await fetch("https://api.spacexdata.com/v3/rockets")).json();
+    let imgs = res.map(ele =>{
+        let {flickr_images} = ele
+        return flickr_images;
+    })
+    return imgs;
+}
+
+const getAllImgShipsAndName = async () =>{
+    let res = await (await fetch("https://api.spacexdata.com/v3/ships")).json();
+    let imgs = res.map(ele =>{
+        let {image, ship_id:id_name} = ele;
+        return {img:image,name_id:id_name};
+    });
+    return imgs;
+}
+
+export default{
+    getAllImgLaunches,
+    getAllImgRockets
 }
 
