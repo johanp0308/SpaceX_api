793a002eaa194c0bbfa38daa25cd1a1bf3cc4672
johanp0308 <sebastian-johan1@hotmail.com>
2023-09-27 07:41:22 -0500

    feat: :beers: Probando querys aun sin termianr
    @johanp0308

---
+++ storages/v4.js
@@ -0,0 +1,19 @@
+
+const getAllImgLaunches = () =>{
+    let query = {
+        
+    }
+    let config = {
+        method: "POST",
+        headers: {"content-type":"application/json"},
+
+    }
+}
+
+
+
+
+
+export default {
+
+}
